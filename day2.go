d5b58720103c190813f5b2ab3c2e25264484c6a3
Erick <boleczek280@outlook.com>
2023-12-05 12:49:17 +0000

    work done

--- day2.go
+++ day2.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 const D2FP = "./files/day2.txt"
@@ -31,21 +34,48 @@ func day2part1() int {
 		pool = append(pool, newGame)
 
 	}
+	for _, game := range pool {
+		colorCheck("red", &game, 12)
+		if &game.possible {
+			colorCheck("green", &game, 13)
+		}
+		if &game.possible {
+			colorCheck("blue", &game, 14)
+		}
+	}
+	
+	count := 100
 
-	return -1
-
-}
-
-func colorCheck(color string, game *Game) {
-	pattern := fmt.Sprintf(`(d+) %s)`, color)
-	re := regexp.MustCompile(pattern)
-
-	matches := re.FindAllStringSubmatch(game.content, -1)
-
-	for _, match := range matches {
-		for _, m := range match {
-			print(m)
+	for _, game := range pool {
+		if !game.possible { 
+			count--
 		}
 	}
 
+	return count
+
+}
+
+func colorCheck(color string, game *Game, limit int) {
+	pattern := fmt.Sprintf(`\d+ %s`, color)
+	re := regexp.MustCompile(pattern)
+
+	matches := re.FindAllStringSubmatch(game.content, -1)
+	biggest := 0
+	// var biggest int
+	for _, match := range matches {
+		parts := strings.Split(match[0], " ")
+		number, err := strconv.Atoi(parts[0])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		if number > biggest {
+			biggest = number
+
+		}
+	}
+	if biggest > limit {
+		game.possible = false
+	}
 }
